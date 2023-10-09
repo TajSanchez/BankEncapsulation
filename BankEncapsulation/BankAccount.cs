8fbbc66ad5b95cd7658b261b7f1b0c2696125d9c
Taj <tajmarie92@gmail.com>
2023-10-09 15:15:00 -0500

    version with if statements

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation;
+public class BankAccount
+{ 
+    public string Name {  get; set; }
+    public string Password { get; set; }
+
+
+    private double _balance;
+    
+
+    public void Deposit(double myMoney)
+    {
+        _balance += myMoney;
+    }
+
+    public double GetBalance()
+    {
+        return _balance;
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace BankEncapsulation
+﻿using BankEncapsulation;
+using System.Runtime.CompilerServices;
+
+var userBalance = new BankAccount();
+userBalance.Deposit(600);
+
+Console.WriteLine("Hello please type your username");
+userBalance.Name = Console.ReadLine();
+
+
+if (userBalance.Name == "Taj")
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine($"Hello {userBalance.Name}, please type your password");
+
+    userBalance.Password = Console.ReadLine();
+}
+
+else if ((userBalance.Name)!= "Taj")
+{
+    Console.WriteLine("Get out");
+}
+
+if (userBalance.Password == "password")
+{
+    Console.WriteLine($"Hello {userBalance.Name}, as of today October 9th, 2023 your account balance is:");
+    Console.WriteLine(userBalance.GetBalance());
+}
+else if (userBalance.Password != "password")
+{
+    Console.WriteLine("Get out");
 }

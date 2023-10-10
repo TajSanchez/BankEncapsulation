05aa88a457d3baa1f64f06e7acd01a791bfa3a30
Taj <tajmarie92@gmail.com>
2023-10-09 19:56:45 -0500

    Added While Loops

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -8,6 +8,7 @@ Console.WriteLine("Hello please type your username");
 userBalance.Name = Console.ReadLine();
 
 
+
 if (userBalance.Name == "Taj")
 {
     Console.WriteLine($"Hello {userBalance.Name}, please type your password");
@@ -15,17 +16,28 @@ if (userBalance.Name == "Taj")
     userBalance.Password = Console.ReadLine();
 }
 
+
 else if ((userBalance.Name)!= "Taj")
 {
     Console.WriteLine("Get out");
 }
+//while (userBalance.Name != "Taj")
+//{
+//    Console.WriteLine("Invalid name, please try again.");
+//    Console.ReadLine();
+//}
+//while (userBalance.Password!= "Password")
+//{
+//    Console.WriteLine("Please try again, please enter your password.");
+//    Console.ReadLine();
+//}
 
-if (userBalance.Password == "password")
+if (userBalance.Password == "Password")
 {
     Console.WriteLine($"Hello {userBalance.Name}, as of today October 9th, 2023 your account balance is:");
     Console.WriteLine(userBalance.GetBalance());
 }
-else if (userBalance.Password != "password")
+else if (userBalance.Password != "Password")
 {
     Console.WriteLine("Get out");
 }
